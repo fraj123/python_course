c81484ebef98910dcd2cfee36b1f543347abf31a
Franz Mejia <mejiafranz@gmail.com>
2022-08-17 16:11:41 -0400

    Quiz staments

--- week2/triangle.py
+++ week2/triangle.py
@@ -7,6 +7,22 @@ def get_seconds(hours, minutes, seconds):
 def month_days(month, days):
     return print(month + " has " + str(days) + " days.")
 
+def order_numbers(number1, number2):
+	if number2 > number1:
+		return number1, number2
+	else:
+		return number2, number1
+
+def lucky_number(name):
+  number = len(name) * 9
+  message = "Hello " + name + ". Your lucky number is " + str(number)
+  print(message)
+
+# 1) Fill in the blanks so the print statement displays the result
+#    of the function call
+smaller, bigger = order_numbers(99, 100)
+print(smaller, bigger)
+
 area_a = area_triangle(5,4)
 area_b = area_triangle(7,3)
 
@@ -23,3 +39,6 @@ print(result)
 
 month_days("June",30)
 month_days("July",31)
+
+lucky_number("Kay")
+lucky_number("Cameron")
